eeb41e7c33c9644125b7af673b2bf1dbf16a23b1
amr <amr8tom@gmail.com>
2024-07-10 07:28:18 +0300

    firstvaildrelease0

--- lib/features/onboarding/presentation/onboarding.dart
+++ lib/features/onboarding/presentation/onboarding.dart
@@ -1,58 +1,52 @@
 import 'package:duaya_app/features/onboarding/managers/onboarding_cubit/onboarding_cubit.dart';
-import 'package:duaya_app/features/onboarding/presentation/widgets/onboarding_dot_navigation.dart';
 import 'package:duaya_app/features/onboarding/presentation/widgets/onboarding_next_button.dart';
 import 'package:duaya_app/features/onboarding/presentation/widgets/onboarding_page.dart';
-import 'package:duaya_app/features/onboarding/presentation/widgets/onboarding_skip.dart';
+import 'package:duaya_app/features/onboarding/presentation/widgets/white_background_contanier.dart';
 import 'package:duaya_app/generated/l10n.dart';
 import 'package:duaya_app/utils/constants/image_strings.dart';
-import 'package:duaya_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../utils/constants/colors.dart';
-
-class OnBoardingScreen extends StatelessWidget {
-  const OnBoardingScreen({super.key});
-
+class OnBoardingScreenTwo extends StatelessWidget {
+  const OnBoardingScreenTwo({super.key});
   @override
   Widget build(BuildContext context) {
     final cubit = context.read<OnBoardingCubit>();
     return Scaffold(
       body: Stack(
         children: [
+          /// White backGround
+          whiteBachGround(),
+
           /// Horizontal Scrollable Page
-          Container(
-            child: PageView(
-              controller: cubit.pageController,
-              onPageChanged: cubit.updatePageIndicator,
-              children: [
-                OnBoardingPage(
-                  image: AssetRes.onBoardingImage1,
-                  title: S.current.onboardingTitle1,
-                  subTitle: S.current.onboardingBody1,
-                ),
-                OnBoardingPage(
-                  image: AssetRes.onBoardingImage2,
-                  title: S.current.onboardingTitle2,
-                  subTitle: S.current.onboardingBody2,
-                ),
-                OnBoardingPage(
-                  image: AssetRes.onBoardingImage3,
-                  title: S.current.onboardingTitle3,
-                  subTitle: S.current.onboardingBody3,
-                ),
-              ],
-            ),
+          PageView(
+            controller: cubit.pageController,
+            onPageChanged: cubit.updatePageIndicator,
+            children: [
+              OnBoardingPage(
+                topHight: 100.h,
+                image: AssetRes.onBoardingImagePng1,
+                title: S.current.onboardingTitle1,
+                subTitle: S.current.onboardingBody1,
+              ),
+              OnBoardingPage(
+                // topHight: 65.h,
+                image: AssetRes.onBoardingImagePng2,
+                title: S.current.onboardingTitle2,
+                subTitle: S.current.onboardingBody2,
+              ),
+              OnBoardingPage(
+                imgWidth: 420.sp,
+                imgHight: 373.sp,
+                image: AssetRes.onBoardingImagePng3,
+                title: S.current.onboardingTitle3,
+                subTitle: S.current.onboardingBody3,
+              ),
+            ],
           ),
 
-          /// Skip Button
-          const OnBoardingSkip(),
-
-          /// Dot Navigation SmoothPageIndicator
-          const OnBoardingDotNavigation(),
-
-          /// Circular Button
+          /// Circular Button and Dot Navigation SmoothPageIndicator
           const OnBoardingNextButton(),
         ],
       ),
